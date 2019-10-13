44e127bda5cc95677c89a9807ddd992a3a21be1e
fatima farajallah <farajallahmfatima@gmail.com>
2019-10-13 14:28:39 +0300

    edit_copy

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 h3.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
